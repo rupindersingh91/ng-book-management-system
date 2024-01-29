001a7b86f3b2f881bfe5c884692654addd2e8223
Rupinder Singh <techformation.rupinder@gmail.com>
2024-01-29 13:47:08 +0530

    Add new book form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 
@@ -10,7 +10,8 @@ import { BookComponent } from './book/book.component';
     BookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class BookComponent {
 
+  newBookTitle: string = "";
+  newBookAuthor: string = "";
+
+  books: Book[] = [];
+
+  /**
+   * Adds a new book to the library.
+   */
+  addBook() {
+    // Check if both title and author are not empty or whitespace
+    if (this.newBookTitle.trim().length && this.newBookAuthor.trim().length) {
+      // Create a new book object
+      let newBook: Book = {
+        id: Date.now(),
+        title: this.newBookTitle,
+        author: this.newBookAuthor
+      };
+      // Add the new book to the library
+      this.books.push(newBook);
+
+      // Reset the input fields
+      this.newBookTitle = "";
+      this.newBookAuthor = "";
+    }
+  }
 }
