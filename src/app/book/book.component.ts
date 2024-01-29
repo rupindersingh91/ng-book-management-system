00b40fea7d41ccf3de127227a73a0a5f93e3133f
Rupinder Singh <techformation.rupinder@gmail.com>
2024-01-29 13:54:08 +0530

    saved books data into local storage

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { Book } from '../models/book.model';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
 
   newBookTitle: string = "";
   newBookAuthor: string = "";
 
   books: Book[] = [];
+
+  /**
+   * Initializes the component when it is first loaded.
+   * Retrieves saved books from local storage and updates the component's state.
+   */
+  ngOnInit(): void {
+    // Retrieve saved books from local storage
+    let savedBooks = localStorage.getItem("books");
+    // Update component's state with saved books, or initialize as empty array if no saved books exist
+    this.books = savedBooks ? JSON.parse(savedBooks) : [];
+  }
 
   /**
    * Adds a new book to the library.
@@ -31,6 +43,9 @@ export class BookComponent {
       // Reset the input fields
       this.newBookTitle = "";
       this.newBookAuthor = "";
+
+      // save books data to local storage
+      localStorage.setItem("books", JSON.stringify(this.books))
     }
   }
 }
